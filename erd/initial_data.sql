1b3e6678f8198ddfc51ed4dce3f6f3f51d0f1f56
Scott Neubauer <scotteneubauer@gmail.com>
2022-10-13 08:49:12 -0500

    Commit progress on xaml and sql

---
+++ erd/initial_data.sql
@@ -0,0 +1,20 @@
+-- populate the machines
+insert into machines (mach_name, mach_loc) values
+	('Robotic Welder', null),
+	('Manual Welder', null),
+	('8N', 4),
+	('250', 9),
+	('350', 13),
+	('HAAS', 6),
+	('NAK', 1),
+	('REX', 10),
+	('NTX', 14),
+	('NZX1', 15),
+	('NZX2', 16),
+	('NZX3', 17);
+
+-- populate the suppliers
+insert into suppliers (supplier_name) values
+	('supplier0'),
+	('supplier1'),
+	('supplier2');
